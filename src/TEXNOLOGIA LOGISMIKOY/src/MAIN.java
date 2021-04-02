cea0503f9984abbce892854596a8ac7d16cfe54b
kleanti bashalli <58070317+kleanthis2001@users.noreply.github.com>
2021-04-03 02:23:24 +0300

    Add files via upload

---
+++ src/TEXNOLOGIA LOGISMIKOY/src/MAIN.java
@@ -0,0 +1,14 @@
+ import javax.swing.JFrame;
+ 
+public class MAIN {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MainFrame frame  =  new MainFrame();
+		/*AdminLogIn frame1 =  new AdminLogIn();
+		EmployeeLogIn fram2 = new EmployeeLogIn();
+		EmployeeScreen kl =  new EmployeeScreen();
+		DeleteEmployee  KL = new DeleteEmployee();*/
+	}
+
+}
